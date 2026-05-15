e00f3ca5791a032710b3e7396a89d0ccde1f6e28
bbbgjdhjggvb <1284454235@qq.com>
2026-05-15 21:08:03 +0800

    兼容数据库中user表格的openid字段

--- service/gateway/auth/model.go
+++ service/gateway/auth/model.go
@@ -7,7 +7,7 @@ import (
 // User 对应数据库中的 users 表
 type User struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
-	OpenID    string    `gorm:"type:varchar(100);not null;uniqueIndex" json:"openid"` // 微信唯一标识，加唯一索引
+	OpenID    string    `gorm:"column:openid;type:varchar(100);not null;uniqueIndex" json:"openid"` // 微信唯一标识，加唯一索引
 	Nickname  string    `gorm:"type:varchar(255)" json:"nickname"`                    // 昵称
 	Status    int       `gorm:"type:tinyint;default:1;comment:'1:正常,0:已取消关注'" json:"status"`
 	CreatedAt time.Time `json:"created_at"`
