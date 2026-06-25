78fccda5f64c8fa1d993be3c54e942cf475fc755
bbbgjdhjggvb <1284454235@qq.com>
2026-06-25 15:29:41 +0800

    添加腾讯云 ICP 备案悬挂

--- frontend/packages/reservation/src/App.vue
+++ frontend/packages/reservation/src/App.vue
@@ -6,5 +6,14 @@ auth.init()
 </script>
 
 <template>
-  <router-view />
+  <div class="min-h-screen flex flex-col">
+    <div class="flex-1">
+      <router-view />
+    </div>
+    <footer class="text-center text-xs text-gray-400 py-3">
+      <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer" class="hover:text-gray-500 transition-colors">
+        ICP备案号：粤ICP备2026072431号
+      </a>
+    </footer>
+  </div>
 </template>
